ca2a2b3086c3fb36a793563d1fdf80245977e3c3
larsbrubaker <larsbrubaker@gmail.com>
2014-02-17 17:02:17 -0800

    Put in more copy write notices put in more gcode defs.

--- examples/GCodeVisualizer/GCodeFile.cs
+++ examples/GCodeVisualizer/GCodeFile.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+Copyright (c) 2014, Lars Brubaker
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -452,11 +481,24 @@ namespace MatterHackers.GCodeVisualizer
                     // wait for bed temperature to be reached
                     break;
 
+                case "200":
+                    // M200 sets the filament diameter.
+                    break;
+
                 case "201":
                     // set axis acceleration
                     break;
 
                 case "204": // - Set default acceleration
+                    break;
+
+                case "207": // M207: calibrate z axis by detecting z max length
+                    break;
+
+                case "208": // M208: set axis max travel
+                    break;
+
+                case "209": // M209: enable automatic retract
                     break;
 
                 case "301":
