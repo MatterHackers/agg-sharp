a705732ebd5b0eeda40e7462f13df5780558235d
james jones <jjones11@g.com>
2014-07-14 10:49:52 -0700

    Making sure we don't put windows off the screen on startup.

--- PlatformWin32/win32/SystemWindowCreator_WindowsForms.cs
+++ PlatformWin32/win32/SystemWindowCreator_WindowsForms.cs
@@ -1,7 +1,37 @@
-﻿using System;
+﻿/*
+Copyright (c) 2014, Lars Brubaker
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MatterHackers.Agg.PlatformAbstract;
 
 using MatterHackers.Agg.UI;
 
@@ -81,6 +111,16 @@ namespace MatterHackers.Agg
         {
             if (systemWindow.Parent != null)
             {
+                // Make sure the window is on screen (this logic should improve over time)
+                position.x = Math.Max(0, position.x);
+                position.y = Math.Max(0, position.y);
+
+                // If it's mac make sure we are not completely under the menu bar.
+                if (OsInformation.OperatingSystem == OSType.Mac)
+                {
+                    position.y = Math.Max(5, position.y);
+                }
+
                 AbstractOsMappingWidget windowsFromsTopWindow = (AbstractOsMappingWidget)systemWindow.Parent;
                 windowsFromsTopWindow.DesktopPosition = position;
             }
