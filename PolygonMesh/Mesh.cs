4b515c7f5c5562855180040c365191db7436b466
larsbrubaker <larsbrubaker@gmail.com>
2014-11-05 11:09:02 -0800

    turn off the aggressive testing

--- PolygonMesh/Mesh.cs
+++ PolygonMesh/Mesh.cs
@@ -26,7 +26,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies, 
 either expressed or implied, of the FreeBSD Project.
 */
-#define AGRESSIVE_VALIDATING
+//#define AGRESSIVE_VALIDATING
 
 using System;
 using System.Collections.Generic;
