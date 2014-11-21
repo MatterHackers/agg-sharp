85ce26d1d2e438ad91d79e56b4ea2c0a2bce0cfd
John Lewin <lewin76@gmail.com>
2014-11-21 11:11:23 -0800

    Assembly loading from Android APK

--- agg/PluginFinder.cs
+++ agg/PluginFinder.cs
@@ -14,7 +14,22 @@ namespace MatterHackers.Agg
         public PluginFinder(string searchDirectory = null, IComparer<BaseClassToFind> sorter = null)
         {
 #if __ANDROID__
-			Plugins = LoadPluginsFromAssets();
+			// Technique for loading directly form Android Assets (Requires you create and populate the Assets->StaticData->Plugins 
+			// folder with the actual plugins you want to laod
+			//Plugins = LoadPluginsFromAssets();
+
+			// Load plugins directly from the APK by supplying the assembly's fully qualified name and allowing the
+			// assembly loader to resolve paths using the built in probing mechanisms
+			Plugins = LoadPluginsFromConfig(new string[]{
+				"PrintNotifications, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null",
+				"MatterControlAuthAndroid, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null",
+				"PlatformAndroid, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null",
+				"AndroidFileDialogs, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"
+				// TODO: Get PictureCreator and TextCreator working on Android
+				/*
+				"PictureCreator, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null",
+				"TextCreator, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"  */
+			});
 #else
 			string searchPath;
 			if (searchDirectory == null)
@@ -37,6 +52,37 @@ namespace MatterHackers.Agg
 
 		private static List<Assembly> pluginAssemblies = null;
 
+		public List<BaseClassToFind> LoadPluginsFromConfig(IEnumerable<string> pluginAssemblyStrings)
+		{
+			List<BaseClassToFind> factoryList = new List<BaseClassToFind>();
+
+			var assets = Android.App.Application.Context.Assets;
+
+			// Only load application plugin assemblies one time
+			if (pluginAssemblies == null) {
+				pluginAssemblies = pluginAssemblyStrings.Select(s => Assembly.Load(s)).ToList();
+			}
+
+			// Iterate plugin assemblies
+			foreach (Assembly assembly in pluginAssemblies)
+			{
+				// Iterate each type
+				foreach (Type type in assembly.GetTypes()) {
+					if (type == null || !type.IsClass || !type.IsPublic) {
+						continue;
+					}
+
+					// Add known/requested types to list
+					if (type.BaseType == typeof(BaseClassToFind)) {
+						factoryList.Add ((BaseClassToFind)Activator.CreateInstance (type));
+					}
+				}
+			}
+
+			return factoryList;
+		}
+
+
 		public List<BaseClassToFind> LoadPluginsFromAssets()
 		{
 			List<BaseClassToFind> factoryList = new List<BaseClassToFind>();
@@ -47,7 +93,6 @@ namespace MatterHackers.Agg
 			if (pluginAssemblies == null) {
 
 				pluginAssemblies = new List<Assembly> ();
-
 				string directory = Path.Combine("StaticData", "Plugins");
 
 				// Iterate the Android Assets in the StaticData/Plugins directory
