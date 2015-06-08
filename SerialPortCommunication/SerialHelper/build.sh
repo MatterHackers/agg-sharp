efc85c5a64ffa32ede7082f8bb8cc524b1f1d314
John Lewin <lewin76@gmail.com>
2015-06-08 14:44:57 -0700

    Correct whitespace

--- SerialPortCommunication/SerialHelper/build.sh
+++ SerialPortCommunication/SerialHelper/build.sh
@@ -2,7 +2,6 @@
 gcc -m32 -shared -fPIC SetSerial.c -o libSetSerial.so -v
 gcc -shared -fPIC SetSerial.c -o libSetSerial -v
 
-
 # Create path if needed
 mkdir -p ../../../../bin/Debug/
 mkdir -p ../../../../bin/Release/
