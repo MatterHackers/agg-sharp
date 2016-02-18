6f169b9648e459741af7266a3c313f97c4cda3c2
Lars Brubaker <larsbrubaker@gmail.com>
2016-02-18 10:43:52 -0800

    Be more aggressive about trying to find the filament diameter then set it to a sane value if it can't be found.

--- examples/GCodeVisualizer/GCodeFileLoaded.cs
+++ examples/GCodeVisualizer/GCodeFileLoaded.cs
@@ -927,12 +927,30 @@ namespace MatterHackers.GCodeVisualizer
 		{
 			if (filamentDiameterCache == 0)
 			{
-				for (int i = 0; i < 20; i++)
+				for (int i = 0; i < Math.Min(20, GCodeCommandQueue.Count); i++)
 				{
 					if (GetFirstNumberAfter("filamentDiameter =", GCodeCommandQueue[i].Line, ref filamentDiameterCache))
 					{
 						break;
 					}
+				}
+
+				if (filamentDiameterCache == 0)
+				{
+					// didn't find it, so look at the end of the file for filament_diameter =
+					string lookFor = "; filament_diameter =";// 2.85
+					for (int i = GCodeCommandQueue.Count - 1; i > Math.Max(0, GCodeCommandQueue.Count - 100); i--)
+					{
+						if (GetFirstNumberAfter(lookFor, GCodeCommandQueue[i].Line, ref filamentDiameterCache))
+						{
+						}
+					}
+				}
+
+				if(filamentDiameterCache == 0)
+				{
+					// it is still 0 so set it to something so we render
+					filamentDiameterCache = 1.75;
 				}
 			}
 
