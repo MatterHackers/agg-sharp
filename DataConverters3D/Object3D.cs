87d9961b06828bcc88e8cb7a5ed8d98521ac43f3
John Lewin <lewin76@gmail.com>
2016-03-29 06:24:19 -0700

    CodeMaid Object3D

--- DataConverters3D/Object3D.cs
+++ DataConverters3D/Object3D.cs
@@ -27,50 +27,36 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
-using ClipperLib;
-using MatterHackers.Agg.VertexSource;
-using MatterHackers.DataConverters2D;
-using MatterHackers.PolygonMesh;
 using MatterHackers.RayTracer;
 using MatterHackers.RayTracer.Traceable;
 using MatterHackers.VectorMath;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MatterHackers.PolygonMesh
 {
-	public class PlatingData
-	{
-		public Vector2 Spacing;
-		public Vector3 CurrentScale = new Vector3(1, 1, 1);
-		public List<IPrimitive> MeshTraceables = new List<IPrimitive>();
-	}
-
 	public enum Object3DTypes { Model, Group, SelectionGroup, GenericObject };
 
 	public interface IObject3D
 	{
-		Object3DTypes ItemType { get; set; }
-		MeshGroup MeshGroup { get; set; }
-		Matrix4X4 Matrix { get; set; }
-		bool Visible { get; set; }
-		bool HasChildren { get; }
-
 		List<IObject3D> Children { get; set; }
-		AxisAlignedBoundingBox GetAxisAlignedBoundingBox();
-		AxisAlignedBoundingBox GetAxisAlignedBoundingBox(Matrix4X4 offet);
-		
-		double DistanceToHit(Ray ray, ref IntersectInfo info);
+		PlatingData ExtraData { get; }
+		bool HasChildren { get; }
+		Object3DTypes ItemType { get; set; }
+		Matrix4X4 Matrix { get; set; }
+		MeshGroup MeshGroup { get; set; }
+		object SourceNode { get; set; } // HACK: For time's sake, stuffing this in for easy persistence
+		bool Visible { get; set; }
 
 		IObject3D Clone();
 
-		PlatingData ExtraData { get; }
-
 		void CreateTraceables();
 
-		// HACK: For time's sake, stuffing this in for easy persistence
-		object SourceNode { get; set; }
+		double DistanceToHit(Ray ray, ref IntersectInfo info);
+
+		AxisAlignedBoundingBox GetAxisAlignedBoundingBox();
+
+		AxisAlignedBoundingBox GetAxisAlignedBoundingBox(Matrix4X4 offet);
 	}
 
 	public static class Object3DExtensions
@@ -90,33 +76,23 @@ namespace MatterHackers.PolygonMesh
 
 	public class Object3D : IObject3D
 	{
+		public List<IObject3D> Children { get; set; } = new List<IObject3D>();
+		public PlatingData ExtraData { get; } = new PlatingData();
 		public bool HasChildren => Children.Count > 0;
 
 		public Object3DTypes ItemType { get; set; } = Object3DTypes.Model;
 
+		public Matrix4X4 Matrix { get; set; } = Matrix4X4.Identity;
+		public MeshGroup MeshGroup { get; set; }
 		public object SourceNode { get; set; }
 
-		/// <summary>
-		/// Initializes trace data, stored in a transformed BVH, for this items mesh and all its children.
-		/// </summary>
-		public void CreateTraceables()
+		public bool Visible { get; set; }
+
+		public static IPrimitive CreateTraceDataForMesh(Mesh mesh)
 		{
-			this.ExtraData.MeshTraceables = new List<IPrimitive> { createTraceables() };
-		}
+			List<IPrimitive> allPolys = AddTraceDataForMesh(mesh);
 
-		private IPrimitive createTraceables()
-		{
-			// Get the trace data for the local mesh
-			List<IPrimitive> meshTraceables = (MeshGroup == null) ? new List<IPrimitive>() : MeshGroup.Meshes.SelectMany(meshGroup => AddTraceDataForMesh(meshGroup)).ToList();
-
-			// Get the trace data for all children
-			foreach (Object3D child in Children)
-			{
-				meshTraceables.Add(child.createTraceables());
-			}
-
-			// Wrap with transform and BVH
-			return new Transform(BoundingVolumeHierarchy.CreateNewHierachy(meshTraceables, 0), Matrix);
+			return BoundingVolumeHierarchy.CreateNewHierachy(allPolys);
 		}
 
 		// TODO - first attempt at deep clone
@@ -129,6 +105,14 @@ namespace MatterHackers.PolygonMesh
 				Children = new List<IObject3D>(this.Children.Select(child => child.Clone())),
 				Matrix = this.Matrix
 			};
+		}
+
+		/// <summary>
+		/// Initializes trace data, stored in a transformed BVH, for this items mesh and all its children.
+		/// </summary>
+		public void CreateTraceables()
+		{
+			this.ExtraData.MeshTraceables = new List<IPrimitive> { createTraceables() };
 		}
 
 		public double DistanceToHit(Ray ray, ref IntersectInfo info)
@@ -144,50 +128,6 @@ namespace MatterHackers.PolygonMesh
 
 			return double.PositiveInfinity;
 		}
-
-		public static IPrimitive CreateTraceDataForMesh(Mesh mesh)
-		{
-			List<IPrimitive> allPolys = AddTraceDataForMesh(mesh);
-
-			return BoundingVolumeHierarchy.CreateNewHierachy(allPolys);
-		}
-
-		private static List<IPrimitive> AddTraceDataForMesh(Mesh mesh)
-		{
-			List<IPrimitive> allPolys = new List<IPrimitive>();
-			List<Vector3> positions = new List<Vector3>();
-
-			foreach (Face face in mesh.Faces)
-			{
-				positions.Clear();
-				foreach (Vertex vertex in face.Vertices())
-				{
-					positions.Add(vertex.Position);
-				}
-
-				// We should use the teselator for this if it is greater than 3.
-				Vector3 next = positions[1];
-				for (int positionIndex = 2; positionIndex < positions.Count; positionIndex++)
-				{
-					TriangleShape triangel = new TriangleShape(positions[0], next, positions[positionIndex], null);
-					allPolys.Add(triangel);
-					next = positions[positionIndex];
-				}
-			}
-
-			return allPolys;
-		}
-
-		
-		public PlatingData ExtraData { get; } = new PlatingData();
-		
-		public List<IObject3D> Children { get; set; } = new List<IObject3D>();
-
-		public Matrix4X4 Matrix { get; set; } = Matrix4X4.Identity;
-
-		public MeshGroup MeshGroup { get; set; }
-
-		public bool Visible { get; set; }
 
 		public AxisAlignedBoundingBox GetAxisAlignedBoundingBox(Matrix4X4 matrix)
 		{
@@ -221,5 +161,52 @@ namespace MatterHackers.PolygonMesh
 			return totalBounds;
 		}
 
+		private static List<IPrimitive> AddTraceDataForMesh(Mesh mesh)
+		{
+			List<IPrimitive> allPolys = new List<IPrimitive>();
+			List<Vector3> positions = new List<Vector3>();
+
+			foreach (Face face in mesh.Faces)
+			{
+				positions.Clear();
+				foreach (Vertex vertex in face.Vertices())
+				{
+					positions.Add(vertex.Position);
+				}
+
+				// We should use the tessellator for this if it is greater than 3.
+				Vector3 next = positions[1];
+				for (int positionIndex = 2; positionIndex < positions.Count; positionIndex++)
+				{
+					TriangleShape triangel = new TriangleShape(positions[0], next, positions[positionIndex], null);
+					allPolys.Add(triangel);
+					next = positions[positionIndex];
+				}
+			}
+
+			return allPolys;
+		}
+
+		private IPrimitive createTraceables()
+		{
+			// Get the trace data for the local mesh
+			List<IPrimitive> meshTraceables = (MeshGroup == null) ? new List<IPrimitive>() : MeshGroup.Meshes.SelectMany(meshGroup => AddTraceDataForMesh(meshGroup)).ToList();
+
+			// Get the trace data for all children
+			foreach (Object3D child in Children)
+			{
+				meshTraceables.Add(child.createTraceables());
+			}
+
+			// Wrap with transform and BVH
+			return new Transform(BoundingVolumeHierarchy.CreateNewHierachy(meshTraceables, 0), Matrix);
+		}
+	}
+
+	public class PlatingData
+	{
+		public Vector3 CurrentScale = new Vector3(1, 1, 1);
+		public List<IPrimitive> MeshTraceables = new List<IPrimitive>();
+		public Vector2 Spacing;
 	}
 }
