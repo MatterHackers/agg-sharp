f30e225cc05c199e68f59e9d554c052a4f92f4b3
John Lewin <lewin76@gmail.com>
2016-04-27 23:52:33 -0700

    Refactor DropDownList
     * Proxy TextColor property to child TextWidget
     * Use initializer syntax
     * Use lambdas for events
     * Remove dead code
     * Gen arrow glyph during construction rather than per draw
     * Consolidate drawing code

--- Gui/Menu/DropDownList.cs
+++ Gui/Menu/DropDownList.cs
@@ -50,19 +50,21 @@ namespace MatterHackers.Agg.UI
 
 		public RGBA_Bytes HoverColor { get; set; }
 
-		private RGBA_Bytes textColor = RGBA_Bytes.Black;
-
 		public RGBA_Bytes TextColor
 		{
-			get { return textColor; }
+			get
+			{
+				return mainControlText.TextColor;
+			}
 			set
 			{
-				textColor = value;
-				mainControlText.TextColor = TextColor;
+				mainControlText.TextColor = value;
 			}
 		}
 
 		private int selectedIndex = -1;
+
+		private PathStorage directionArrow = null;
 
 		public int SelectedIndex
 		{
@@ -99,7 +101,7 @@ namespace MatterHackers.Agg.UI
 			}
 		}
 
-		public String SelectedLabel
+		public string SelectedLabel
 		{
 			get
 			{
@@ -131,7 +133,7 @@ namespace MatterHackers.Agg.UI
 			}
 		}
 
-		public String SelectedValue
+		public string SelectedValue
 		{
 			get
 			{
@@ -168,16 +170,39 @@ namespace MatterHackers.Agg.UI
 		public DropDownList(string noSelectionString, RGBA_Bytes normalColor, RGBA_Bytes hoverColor, Direction direction = Direction.Down, double maxHeight = 0)
 			: base(direction, maxHeight)
 		{
-			MenuItems.CollectionChanged += new NotifyCollectionChangedEventHandler(MenuItems_CollectionChanged);
-			mainControlText = new TextWidget(noSelectionString);
-			mainControlText.AutoExpandBoundsToText = true;
-			mainControlText.VAnchor = UI.VAnchor.ParentBottom | UI.VAnchor.FitToChildren;
-			mainControlText.HAnchor = UI.HAnchor.ParentLeft | UI.HAnchor.FitToChildren;
-			mainControlText.Margin = new BorderDouble(right: 30);
+			if (this.MenuDirection == Direction.Down)
+			{
+				directionArrow = new PathStorage();
+				directionArrow.MoveTo(-4, 0);
+				directionArrow.LineTo(4, 0);
+				directionArrow.LineTo(0, -5);
+			}
+			else if (this.MenuDirection == Direction.Up)
+			{
+				directionArrow = new PathStorage();
+				directionArrow.MoveTo(-4, -5);
+				directionArrow.LineTo(4, -5);
+				directionArrow.LineTo(0, 0);
+			}
+			else
+			{
+				throw new NotImplementedException("Pulldown direction has not been implemented");
+			}
+
+			MenuItems.CollectionChanged += MenuItems_CollectionChanged;
+
+			mainControlText = new TextWidget(noSelectionString)
+			{
+				AutoExpandBoundsToText = true,
+				VAnchor = VAnchor.ParentBottom | VAnchor.FitToChildren,
+				HAnchor = HAnchor.ParentLeft | HAnchor.FitToChildren,
+				Margin = new BorderDouble(right: 30),
+				TextColor = RGBA_Bytes.Black
+			};
 			AddChild(mainControlText);
 
-			MouseEnter += new EventHandler(DropDownList_MouseEnter);
-			MouseLeave += new EventHandler(DropDownList_MouseLeave);
+			MouseEnter += (s, e) => BackgroundColor = HoverColor;
+			MouseLeave += (s, e) => BackgroundColor = NormalColor;
 
 			NormalColor = normalColor;
 			HoverColor = hoverColor;
@@ -185,22 +210,9 @@ namespace MatterHackers.Agg.UI
 			BorderColor = RGBA_Bytes.White;
 		}
 
-		private void DropDownList_MouseLeave(object sender, EventArgs e)
-		{
-			BackgroundColor = NormalColor;
-		}
-
-		private void DropDownList_MouseEnter(object sender, EventArgs e)
-		{
-			BackgroundColor = HoverColor;
-		}
-
 		private void OnSelectionChanged(EventArgs e)
 		{
-			if (SelectionChanged != null)
-			{
-				SelectionChanged(this, e);
-			}
+			SelectionChanged?.Invoke(this, e);
 		}
 
 		private void MenuItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -222,77 +234,34 @@ namespace MatterHackers.Agg.UI
 			foreach (MenuItem item in e.NewItems)
 			{
 				item.MinimumSize = minSize;
-				item.Selected -= new EventHandler(item_Selected);
-				item.Selected += new EventHandler(item_Selected);
-			}
-		}
-
-		private void SetMenuItemsToNewMinSIze()
-		{
-			Vector2 minSize = new Vector2(LocalBounds.Width, LocalBounds.Height);
-
-			foreach (MenuItem item in MenuItems)
-			{
-				item.MinimumSize = new Vector2(LocalBounds.Width, LocalBounds.Height);
 			}
 		}
 
 		public override void OnBoundsChanged(EventArgs e)
 		{
-			SetMenuItemsToNewMinSIze();
-			base.OnBoundsChanged(e);
-		}
-
-		private void item_Selected(object sender, EventArgs e)
-		{
-			int newSelectedIndex = 0;
+			// Set new MinSIze
+			Vector2 minSize = new Vector2(LocalBounds.Width, LocalBounds.Height);
 			foreach (MenuItem item in MenuItems)
 			{
-				if (item == sender)
-				{
-					break;
-				}
-				newSelectedIndex++;
+				item.MinimumSize = new Vector2(LocalBounds.Width, LocalBounds.Height);
 			}
 
-			SelectedIndex = newSelectedIndex;
+			base.OnBoundsChanged(e);
 		}
 
 		public override void OnDraw(Graphics2D graphics2D)
 		{
 			base.OnDraw(graphics2D);
-			this.DrawBorder(graphics2D);
-			this.DrawDirectionalArrow(graphics2D);
-		}
 
-		private void DrawDirectionalArrow(Graphics2D graphics2D)
-		{
-			PathStorage littleArrow = new PathStorage();
-			if (this.MenuDirection == Direction.Down)
-			{
-				littleArrow.MoveTo(-4, 0);
-				littleArrow.LineTo(4, 0);
-				littleArrow.LineTo(0, -5);
-			}
-			else if (this.MenuDirection == Direction.Up)
-			{
-				littleArrow.MoveTo(-4, -5);
-				littleArrow.LineTo(4, -5);
-				littleArrow.LineTo(0, 0);
-			}
-			else
-			{
-				throw new NotImplementedException("Pulldown direction has not been implemented");
-			}
-			graphics2D.Render(littleArrow, LocalBounds.Right - 8, LocalBounds.Top - 4, BorderColor);
-		}
-
-		private void DrawBorder(Graphics2D graphics2D)
-		{
-			RectangleDouble Bounds = LocalBounds;
-			RoundedRect borderRect = new RoundedRect(this.LocalBounds, 0);
-			Stroke strokeRect = new Stroke(borderRect, BorderWidth);
+			// Draw border
+			var strokeRect = new Stroke(new RoundedRect(this.LocalBounds, 0), BorderWidth);
 			graphics2D.Render(strokeRect, BorderColor);
+
+			// Draw directional arrow
+			if (directionArrow != null)
+			{
+				graphics2D.Render(directionArrow, LocalBounds.Right - 8, LocalBounds.Top - 4, BorderColor);
+			}
 		}
 
 		public bool UseLeftIcons { get; set; } = true;
@@ -302,7 +271,6 @@ namespace MatterHackers.Agg.UI
 			mainControlText.Margin = MenuItemsPadding;
 
 			GuiWidget normalTextWithMargin = GetMenuContent(itemName, leftImage, MenuItemsBackgroundColor);
-
 			GuiWidget hoverTextWithMargin = GetMenuContent(itemName, leftImage, MenuItemsBackgroundHoverColor);
 
 			MenuItem menuItem = new MenuItem(
@@ -319,32 +287,39 @@ namespace MatterHackers.Agg.UI
 
 		private GuiWidget GetMenuContent(string itemName, ImageBuffer leftImage, RGBA_Bytes color)
 		{
-			GuiWidget normalTextWithMargin = new FlowLayoutWidget();
-			normalTextWithMargin.HAnchor = UI.HAnchor.ParentLeftRight | UI.HAnchor.FitToChildren;
-			normalTextWithMargin.VAnchor = UI.VAnchor.FitToChildren;
-			normalTextWithMargin.BackgroundColor = color;
-			TextWidget normal = new TextWidget(itemName);
-			normal.Margin = MenuItemsPadding;
-			normal.TextColor = MenuItemsTextColor;
+			var rowContainer = new FlowLayoutWidget()
+			{
+				HAnchor = HAnchor.ParentLeftRight | HAnchor.FitToChildren,
+				VAnchor = VAnchor.FitToChildren,
+				BackgroundColor = color
+			};
+
+			var textWidget = new TextWidget(itemName)
+			{
+				Margin = MenuItemsPadding,
+				TextColor = MenuItemsTextColor
+			};
+
 			if (UseLeftIcons)
 			{
 				if (leftImage != null)
 				{
 					ImageBuffer scaledImage = ImageBuffer.CreateScaledImage(leftImage, 20, 20);
-					normalTextWithMargin.AddChild(new ImageWidget(scaledImage)
+					rowContainer.AddChild(new ImageWidget(scaledImage)
 					{
 						VAnchor = VAnchor.ParentCenter,
 						Margin = new BorderDouble(MenuItemsPadding.Left, MenuItemsPadding.Bottom, 3, MenuItemsPadding.Top),
 					});
-					normal.Margin = new BorderDouble(0, MenuItemsPadding.Bottom, MenuItemsPadding.Right, MenuItemsPadding.Top);
+					textWidget.Margin = new BorderDouble(0, MenuItemsPadding.Bottom, MenuItemsPadding.Right, MenuItemsPadding.Top);
 				}
 				else
 				{
-					normal.Margin = new BorderDouble(MenuItemsPadding.Left + 20 + 3, MenuItemsPadding.Bottom, MenuItemsPadding.Right, MenuItemsPadding.Top);
+					textWidget.Margin = new BorderDouble(MenuItemsPadding.Left + 20 + 3, MenuItemsPadding.Bottom, MenuItemsPadding.Right, MenuItemsPadding.Top);
 				}
 			}
-			normalTextWithMargin.AddChild(normal);
-			return normalTextWithMargin;
+			rowContainer.AddChild(textWidget);
+
+			return rowContainer;
 		}
 	}
 }
