bfa4dca94528968a7bc6726d8bd62b20c7544470
Lars Brubaker <larsbrubaker@gmail.com>
2016-04-29 18:14:12 -0700

    Made  a new MenuItemColorStatesView that only changes colors.

--- Gui/Menu/MenuItemStatesView.cs
+++ Gui/Menu/MenuItemStatesView.cs
@@ -67,7 +67,117 @@ namespace MatterHackers.Agg.UI
 			// Find the FlowLayoutWidget containing this MenuItemStatesView  instance
 			var dropListContainer = this.Parents<FlowLayoutWidget>().FirstOrDefault();
 
-			// Loop over any sibling MenuItem wigets
+			// Loop over any sibling MenuItem widgets
+			foreach (var menuItem in dropListContainer.Children<MenuItem>())
+			{
+				// Find the MenuItemStatesView instance that they contain and set highlighted to false
+				var statesView = menuItem.Children<MenuItemStatesView>().FirstOrDefault();
+				if (statesView != null)
+				{
+					statesView.Highlighted = false;
+				}
+			}
+		}
+	}
+
+	public class MenuItemColorStatesView : GuiWidget
+	{
+		GuiWidget textWidgetContainer;
+		TextWidget textWidget;
+
+		public RGBA_Bytes NormalBackgroundColor { get; set; }
+		public RGBA_Bytes OverBackgroundColor { get; set; }
+		public RGBA_Bytes DisabledBackgroundColor { get; set; }
+
+		public RGBA_Bytes NormalTextColor { get; set; }
+		public RGBA_Bytes OverTextColor { get; set; }
+		public RGBA_Bytes DisabledTextColor { get; set; }
+
+		public double PointSize { get { return textWidget.PointSize; } set { textWidget.PointSize = value; } }
+
+		public MenuItemColorStatesView(string name)
+		{
+			HAnchor = HAnchor.ParentLeftRight | HAnchor.FitToChildren;
+			VAnchor = VAnchor.FitToChildren;
+			Selectable = false;
+
+			textWidgetContainer = new GuiWidget();
+			textWidget = new TextWidget(name)
+			{
+				AutoExpandBoundsToText = true,
+			};
+
+			textWidgetContainer.HAnchor = HAnchor.FitToChildren;
+			textWidgetContainer.AddChild(textWidget);
+			textWidgetContainer.VAnchor = VAnchor.FitToChildren;
+
+			AddChild(textWidgetContainer);
+		}
+
+		public override void OnParentChanged(EventArgs ex)
+		{
+			// We don't need to remove these as the parent we are attached to is the held list that gets turned
+			// into the menu list when required and unhooking these breaks that list from working.
+			// This will get cleared when the list is no longer need and the menu (the parent) is removed)
+			Parent.MouseLeave += (s, e) => this.Highlighted = false;
+			Parent.MouseEnter += (s, e) =>
+			{
+				ClearActiveHighlight();
+				this.Highlighted = true;
+			};
+
+			Parent.EnabledChanged += (s, e) =>
+			{
+				if (Parent.Enabled)
+				{
+					textWidget.TextColor = NormalTextColor;
+				}
+				else
+				{
+					textWidget.TextColor = DisabledTextColor;
+				}
+			};
+
+			base.OnParentChanged(ex);
+		}
+
+		public override void SendToChildren(object objectToRout)
+		{
+			if (objectToRout as MenuItem.MenuClosedMessage != null)
+			{
+				this.Highlighted = false;
+			}
+
+			base.SendToChildren(objectToRout);
+		}
+
+		public bool Highlighted
+		{
+			get
+			{
+				return BackgroundColor == OverBackgroundColor;
+			}
+			set
+			{
+				if(value)
+				{
+					BackgroundColor = OverBackgroundColor;
+					textWidget.TextColor = OverTextColor;
+				}
+				else
+				{
+					BackgroundColor = NormalBackgroundColor;
+					textWidget.TextColor = NormalTextColor;
+				}
+			}
+		}
+
+		private void ClearActiveHighlight()
+		{
+			// Find the FlowLayoutWidget containing this MenuItemStatesView  instance
+			var dropListContainer = this.Parents<FlowLayoutWidget>().FirstOrDefault();
+
+			// Loop over any sibling MenuItem widgets
 			foreach (var menuItem in dropListContainer.Children<MenuItem>())
 			{
 				// Find the MenuItemStatesView instance that they contain and set highlighted to false
