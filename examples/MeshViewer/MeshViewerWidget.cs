a817a5442ad70479f5f0834bb6dd3f4364e99473
Lars Brubaker <larsbrubaker@gmail.com>
2016-05-12 10:50:03 -0700

    make the bed grid lines not change until the bed is > 300mm

--- examples/MeshViewer/MeshViewerWidget.cs
+++ examples/MeshViewer/MeshViewerWidget.cs
@@ -325,7 +325,7 @@ namespace MatterHackers.MeshVisualizer
 				divisor = 100;
 				skip = 10;
 			}
-			else if (sizeForMarking > 250)
+			else if (sizeForMarking > 300)
 			{
 				divisor = 50;
 				skip = 5;
