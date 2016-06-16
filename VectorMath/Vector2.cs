02f057d6f7ba993ec5206b1857cf82721bd973a2
larsbrubaker <larsbrubaker@gmail.com>
2016-06-16 06:58:13 -0700

    Made Vector2 IConvertible

--- VectorMath/Vector2.cs
+++ VectorMath/Vector2.cs
@@ -32,7 +32,7 @@ namespace MatterHackers.VectorMath
 	/// <summary>Represents a 2D vector using two double-precision floating-point numbers.</summary>
 	[Serializable]
 	[StructLayout(LayoutKind.Sequential)]
-	public struct Vector2 : IEquatable<Vector2>
+	public struct Vector2 : IEquatable<Vector2> , IConvertible
 	{
 		#region Fields
 
@@ -927,5 +927,93 @@ namespace MatterHackers.VectorMath
 		}
 
 		#endregion IEquatable<Vector2d> Members
+
+		#region IConvertable
+		public TypeCode GetTypeCode()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ToBoolean(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public char ToChar(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public sbyte ToSByte(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte ToByte(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public short ToInt16(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ushort ToUInt16(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ToInt32(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public uint ToUInt32(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long ToInt64(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ulong ToUInt64(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public float ToSingle(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double ToDouble(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public decimal ToDecimal(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DateTime ToDateTime(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ToString(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ToType(Type conversionType, IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion IConvertable
 	}
 }
