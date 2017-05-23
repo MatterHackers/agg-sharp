f5c480755f063831afaaff652dc6c4865f015536
Lars Brubaker <larsbrubaker@gmail.com>
2017-05-23 15:05:43 -0700

    Fixed a bug with triangle polygons ray tracing
    Put in new tests for validation.

--- examples/RayTracerTest/RayTracerWiget.cs
+++ examples/RayTracerTest/RayTracerWiget.cs
@@ -336,7 +336,8 @@ namespace MatterHackers.RayTracer
 			Stopwatch loadTime = new Stopwatch();
 			loadTime.Start();
 
-			PolygonMesh.Mesh simpleMesh = StlProcessing.Load("Simple.stl");
+			PolygonMesh.Mesh simpleMesh = StlProcessing.Load("circle_100x100_centered.stl");
+			//PolygonMesh.Mesh simpleMesh = StlProcessing.Load("Simple.stl");
 			//PolygonMesh.Mesh simpleMesh = StlProcessing.Load("Complex.stl");
 			//PolygonMesh.Mesh simpleMesh = StlProcessing.Load("bunny.stl");
 			//PolygonMesh.Mesh simpleMesh = StlProcessing.Load("bunny_binary.stl");
