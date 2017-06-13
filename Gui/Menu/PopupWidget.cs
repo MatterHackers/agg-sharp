d88b068cf3b65a8067863fc21a9f3253921945a9
Lars Brubaker <larsbrubaker@gmail.com>
2017-06-13 14:31:10 -0700

    Making it possible to have layout engines for popup windows.

--- Gui/Menu/PopupWidget.cs
+++ Gui/Menu/PopupWidget.cs
@@ -6,12 +6,310 @@ using MatterHackers.VectorMath;
 
 namespace MatterHackers.Agg.UI
 {
+	/// <summary>
+	/// Marker interface for ignoring mouse input on popup widget children
+	/// </summary>
+	public interface IIgnoredPopupChild
+	{
+	}
+
+	public interface IPopupLayoutEngine
+	{
+		double MaxHeight { get; }
+
+		void Closed();
+
+		void ShowPopup(PopupWidget popupWidget);
+	}
+
+	public class PopupWidget : GuiWidget
+	{
+		private GuiWidget contentWidget;
+		private IPopupLayoutEngine layoutEngine;
+		private ScrollableWidget scrollingWindow;
+		private Vector2 scrollPositionAtMouseDown;
+		private Vector2 scrollPositionAtMouseUp;
+
+		public PopupWidget(GuiWidget contentWidget, IPopupLayoutEngine layoutEngine)
+		{
+			this.contentWidget = contentWidget;
+
+			this.layoutEngine = layoutEngine;
+
+			ignoredWidgets = contentWidget.Children.Where(c => c is IIgnoredPopupChild).ToList();
+
+			if (contentWidget is IIgnoredPopupChild)
+			{
+				ignoredWidgets.Add(contentWidget);
+			}
+
+			scrollingWindow = new ScrollableWidget(true);
+			{
+				contentWidget.ClearRemovedFlag();
+				scrollingWindow.AddChild(contentWidget);
+
+				contentWidget.HAnchor = UI.HAnchor.ParentLeft | UI.HAnchor.FitToChildren;
+				contentWidget.VAnchor = UI.VAnchor.ParentBottom;
+				Width = contentWidget.Width;
+				Height = contentWidget.Height;
+			}
+
+			scrollingWindow.HAnchor = HAnchor.ParentLeftRight;
+			scrollingWindow.VAnchor = VAnchor.ParentBottomTop;
+			if (layoutEngine.MaxHeight > 0 && Height > layoutEngine.MaxHeight)
+			{
+				MakeMenuHaveScroll(layoutEngine.MaxHeight);
+			}
+			AddChild(scrollingWindow);
+
+			layoutEngine.ShowPopup(this);
+		}
+
+		public RGBA_Bytes BorderColor { get; set; }
+
+		public int BorderWidth { get; set; }
+		private List<GuiWidget> ignoredWidgets { get; }
+
+		public virtual void CloseMenu()
+		{
+			this.contentWidget?.Parent?.RemoveChild(this.contentWidget);
+			this.contentWidget.ClearRemovedFlag();
+
+			this.Parent?.RemoveChild(this);
+			this.Close();
+		}
+
+		public override void OnClosed(ClosedEventArgs e)
+		{
+			layoutEngine.Closed();
+
+			base.OnClosed(e);
+		}
+
+		public override void OnContainsFocusChanged(EventArgs e)
+		{
+			UiThread.RunOnIdle(() =>
+			{
+				// Fired any time focus changes. Traditionally we closed the menu if the we weren't focused.
+				// To accommodate children (or external widgets) having focus we also query for and consider special cases
+				bool specialChildHasFocus = ignoredWidgets.Any(w => w.ContainsFocus || w.Focused)
+					|| this.ChildrenRecursive<DropDownList>().Any(w => w.IsOpen);
+
+				// If the focused changed and we've lost focus and no special cases permit, close the menu
+				if (!this.ContainsFocus
+					&& !specialChildHasFocus)
+				{
+					UiThread.RunOnIdle(CloseMenu);
+				}
+			});
+
+			base.OnContainsFocusChanged(e);
+		}
+
+		public override void OnDraw(Graphics2D graphics2D)
+		{
+			base.OnDraw(graphics2D);
+
+			var outline = new RoundedRect(LocalBounds, 0);
+			graphics2D.Render(new Stroke(outline, BorderWidth * 2), BorderColor);
+		}
+
+		public override void OnMouseDown(MouseEventArgs mouseEvent)
+		{
+			scrollPositionAtMouseDown = scrollingWindow.ScrollPosition;
+			base.OnMouseDown(mouseEvent);
+		}
+
+		public override void OnMouseUp(MouseEventArgs mouseEvent)
+		{
+			bool mouseUpOnIgnoredChild = ignoredWidgets.Any(w => w.MouseCaptured || w.ChildHasMouseCaptured);
+
+			bool clickIsInsideScrollArea = (scrollingWindow?.ScrollArea?.Children?[0]?.ChildHasMouseCaptured == true);
+
+			scrollPositionAtMouseUp = scrollingWindow.ScrollPosition;
+			if (!scrollingWindow.VerticalScrollBar.ChildHasMouseCaptured
+				&& AllowClickingItems()
+				&& clickIsInsideScrollArea
+				&& !mouseUpOnIgnoredChild)
+			{
+				UiThread.RunOnIdle(CloseMenu);
+			}
+			base.OnMouseUp(mouseEvent);
+		}
+
+		public void ScrollIntoView(GuiWidget widget)
+		{
+			if (scrollingWindow.VerticalScrollBar.Visible)
+			{
+				scrollingWindow.ScrollPosition = new Vector2(0, -widget.BoundsRelativeToParent.Bottom + this.Height / 2);
+			}
+		}
+
+		/// <summary>
+		/// Filter to allow click events as long as the scroll position is less than the given threshhold. Prevent click behavior on touch platfroms when drag scrolling
+		/// </summary>
+		/// <returns>A bool indicating if scroll distance is within tolerance</returns>
+		internal bool AllowClickingItems()
+		{
+			return (scrollPositionAtMouseDown - scrollPositionAtMouseUp).Length <= 5;
+		}
+
+		internal void MakeMenuHaveScroll(double maxHeight)
+		{
+			scrollingWindow.VAnchor = VAnchor.AbsolutePosition;
+			scrollingWindow.Height = maxHeight;
+			scrollingWindow.MinimumSize = new Vector2(Width + 15, 0);
+			Width = scrollingWindow.Width;
+			Height = maxHeight;
+			scrollingWindow.ScrollArea.VAnchor = VAnchor.FitToChildren;
+		}
+	}
+
+	public class PopupLayoutEngine : IPopupLayoutEngine
+	{
+		protected GuiWidget widgetRelativeTo;
+		private bool alignToRightEdge;
+		private GuiWidget contentWidget;
+		private Direction direction;
+		private bool firstTimeSizing = true;
+		private HashSet<GuiWidget> hookedParents = new HashSet<GuiWidget>();
+		private Vector2 openOffset;
+		private PopupWidget popupWidget;
+
+		public PopupLayoutEngine(GuiWidget contentWidget, GuiWidget widgetRelativeTo, Vector2 openOffset, Direction direction, double maxHeight, bool alignToRightEdge)
+		{
+			this.MaxHeight = maxHeight;
+			this.contentWidget = contentWidget;
+			this.alignToRightEdge = alignToRightEdge;
+			this.openOffset = openOffset;
+			this.direction = direction;
+			this.widgetRelativeTo = widgetRelativeTo;
+		}
+
+		public double MaxHeight { get; private set; }
+
+		public void Closed()
+		{
+			// Unbind callbacks on parents for position_changed if we're closing
+			foreach (GuiWidget widget in hookedParents)
+			{
+				widget.PositionChanged -= widgetRelativeTo_PositionChanged;
+				widget.BoundsChanged -= widgetRelativeTo_PositionChanged;
+			}
+
+			// Long lived originating item must be unregistered
+			widgetRelativeTo.Closed -= widgetRelativeTo_Closed;
+
+			// Restore focus to originating widget on close
+			if (this.widgetRelativeTo != null
+				&& !widgetRelativeTo.HasBeenClosed)
+			{
+				// On menu close, select the first scrollable parent of the widgetRelativeTo
+				var scrollableParent = widgetRelativeTo.Parents<ScrollableWidget>().FirstOrDefault();
+				if (scrollableParent != null)
+				{
+					scrollableParent.Focus();
+				}
+			}
+		}
+
+		public void ShowPopup(PopupWidget popupWidget)
+		{
+			this.popupWidget = popupWidget;
+			SystemWindow windowToAddTo = widgetRelativeTo.Parents<SystemWindow>().FirstOrDefault();
+			windowToAddTo?.AddChild(popupWidget);
+
+			GuiWidget topParent = widgetRelativeTo.Parent;
+			while (topParent.Parent != null
+				&& topParent as SystemWindow == null)
+			{
+				// Regrettably we don't know who it is that is the window that will actually think it is moving relative to its parent
+				// but we need to know anytime our widgetRelativeTo has been moved by any change, so we hook them all.
+				if (!hookedParents.Contains(topParent))
+				{
+					hookedParents.Add(topParent);
+					topParent.PositionChanged += widgetRelativeTo_PositionChanged;
+					topParent.BoundsChanged += widgetRelativeTo_PositionChanged;
+				}
+
+				topParent = topParent.Parent;
+			}
+
+			widgetRelativeTo_PositionChanged(widgetRelativeTo, null);
+			widgetRelativeTo.Closed += widgetRelativeTo_Closed;
+		}
+
+		private void widgetRelativeTo_Closed(object sender, ClosedEventArgs e)
+		{
+			// If the owning widget closed, so should we
+			popupWidget.CloseMenu();
+		}
+
+		private void widgetRelativeTo_PositionChanged(object sender, EventArgs e)
+		{
+			if (widgetRelativeTo != null)
+			{
+				Vector2 zero = widgetRelativeTo.OriginRelativeParent;
+				if (alignToRightEdge)
+				{
+					zero += new Vector2(widgetRelativeTo.LocalBounds.Left, widgetRelativeTo.LocalBounds.Bottom);
+				}
+
+				GuiWidget topParent = widgetRelativeTo.Parent;
+				while (topParent != null && topParent.Parent != null)
+				{
+					topParent.ParentToChildTransform.transform(ref zero);
+					topParent = topParent.Parent;
+				}
+
+				double alignmentOffset = 0;
+				if (alignToRightEdge)
+				{
+					alignmentOffset = -popupWidget.Width + widgetRelativeTo.Width;
+				}
+
+				if (firstTimeSizing)
+				{
+					var maxAllowed = 25;
+					double distanceToWindowBottom = zero.y - popupWidget.Height;
+					if (distanceToWindowBottom < 0)
+					{
+						if (popupWidget.Height + distanceToWindowBottom > maxAllowed)
+						{
+							popupWidget.MakeMenuHaveScroll(popupWidget.Height + distanceToWindowBottom - 5);
+						}
+						else
+						{
+							direction = Direction.Up;
+						}
+					}
+				}
+
+				switch (direction)
+				{
+					case Direction.Down:
+						popupWidget.OriginRelativeParent = zero + new Vector2(alignmentOffset, -popupWidget.Height) + openOffset;
+						break;
+
+					case Direction.Up:
+						popupWidget.OriginRelativeParent = zero + new Vector2(alignmentOffset, widgetRelativeTo.Height) + openOffset;
+						break;
+
+					default:
+						throw new NotImplementedException();
+				}
+			}
+
+			firstTimeSizing = false;
+		}
+	}
+
 	internal class PopupMenu : PopupWidget
 	{
 		private List<MenuItem> MenuItems;
 
 		public PopupMenu(IEnumerable<MenuItem> MenuItems, GuiWidget popupContent, GuiWidget widgetRelativeTo, Vector2 openOffset, Direction direction, double maxHeight, bool alignToRightEdge)
-			: base(popupContent, widgetRelativeTo, openOffset, direction, maxHeight, alignToRightEdge)
+			: base(popupContent, new PopupLayoutEngine(popupContent, widgetRelativeTo, openOffset, direction, maxHeight, alignToRightEdge))
 		{
 			this.Name = "_OpenMenuContents";
 			this.MenuItems = new List<MenuItem>();
@@ -48,269 +346,5 @@ namespace MatterHackers.Agg.UI
 
 			base.OnClosed(e);
 		}
-	}
-
-	public class PopupWidget : GuiWidget
-	{
-		public RGBA_Bytes BorderColor { get; set; }
-
-		public int BorderWidth { get; set; }
-
-		private bool firstTimeSizing = true;
-		private bool alignToRightEdge;
-
-		private Direction direction;
-		
-		private Vector2 openOffset;
-		
-		private Vector2 scrollPositionAtMouseDown;
-		private Vector2 scrollPositionAtMouseUp;
-
-		protected GuiWidget widgetRelativeTo;
-		private GuiWidget contentWidget;
-		private ScrollableWidget scrollingWindow;
-		private List<GuiWidget> ignoredWidgets { get; }
-		private HashSet<GuiWidget> hookedParents = new HashSet<GuiWidget>();
-
-		public PopupWidget(GuiWidget contentWidget, GuiWidget widgetRelativeTo, Vector2 openOffset, Direction direction, double maxHeight, bool alignToRightEdge)
-		{
-			this.alignToRightEdge = alignToRightEdge;
-			this.openOffset = openOffset;
-			this.contentWidget = contentWidget;
-
-			ignoredWidgets = contentWidget.Children.Where(c => c is IIgnoredPopupChild).ToList();
-
-			if (contentWidget is IIgnoredPopupChild)
-			{
-				ignoredWidgets.Add(contentWidget);
-			}
-
-			this.direction = direction;
-			this.widgetRelativeTo = widgetRelativeTo;
-
-			scrollingWindow = new ScrollableWidget(true);
-			{
-				contentWidget.ClearRemovedFlag();
-				scrollingWindow.AddChild(contentWidget);
-
-				contentWidget.HAnchor = UI.HAnchor.ParentLeft | UI.HAnchor.FitToChildren;
-				contentWidget.VAnchor = UI.VAnchor.ParentBottom;
-				Width = contentWidget.Width;
-				Height = contentWidget.Height;
-			}
-
-			scrollingWindow.HAnchor = HAnchor.ParentLeftRight;
-			scrollingWindow.VAnchor = VAnchor.ParentBottomTop;
-			if (maxHeight > 0 && Height > maxHeight)
-			{
-				MakeMenuHaveScroll(maxHeight);
-			}
-			AddChild(scrollingWindow);
-
-			GuiWidget topParent = widgetRelativeTo.Parent;
-			while (topParent.Parent != null
-				&& topParent as SystemWindow == null)
-			{
-				// Regrettably we don't know who it is that is the window that will actually think it is moving relative to its parent
-				// but we need to know anytime our widgetRelativeTo has been moved by any change, so we hook them all.
-				if (!hookedParents.Contains(topParent))
-				{
-					hookedParents.Add(topParent);
-					topParent.PositionChanged += widgetRelativeTo_PositionChanged;
-					topParent.BoundsChanged += widgetRelativeTo_PositionChanged;
-				}
-
-				topParent = topParent.Parent;
-			}
-			topParent.AddChild(this);
-
-			widgetRelativeTo_PositionChanged(widgetRelativeTo, null);
-			widgetRelativeTo.Closed += widgetRelativeTo_Closed;
-		}
-
-		public void ScrollIntoView(GuiWidget widget)
-		{
-			if (scrollingWindow.VerticalScrollBar.Visible)
-			{
-				scrollingWindow.ScrollPosition = new Vector2(0, -widget.BoundsRelativeToParent.Bottom + this.Height /2);
-			}
-		}
-
-		private void MakeMenuHaveScroll(double maxHeight)
-		{
-			scrollingWindow.VAnchor = VAnchor.AbsolutePosition;
-			scrollingWindow.Height = maxHeight;
-			scrollingWindow.MinimumSize = new Vector2(Width + 15, 0);
-			Width = scrollingWindow.Width;
-			Height = maxHeight;
-			scrollingWindow.ScrollArea.VAnchor = VAnchor.FitToChildren;
-		}
-
-		public override void OnDraw(Graphics2D graphics2D)
-		{
-			base.OnDraw(graphics2D);
-
-			var outline = new RoundedRect(LocalBounds, 0);
-			graphics2D.Render(new Stroke(outline, BorderWidth * 2), BorderColor);
-		}
-
-		private void widgetRelativeTo_PositionChanged(object sender, EventArgs e)
-		{
-			if (widgetRelativeTo != null)
-			{
-				Vector2 zero = widgetRelativeTo.OriginRelativeParent;
-				if (alignToRightEdge)
-				{
-					zero += new Vector2(widgetRelativeTo.LocalBounds.Left, widgetRelativeTo.LocalBounds.Bottom);
-				}
-
-				GuiWidget topParent = widgetRelativeTo.Parent;
-				while (topParent != null && topParent.Parent != null)
-				{
-					topParent.ParentToChildTransform.transform(ref zero);
-					topParent = topParent.Parent;
-				}
-
-				double alignmentOffset = 0;
-				if (alignToRightEdge)
-				{
-					alignmentOffset = -Width + widgetRelativeTo.Width;
-				}
-
-				if (firstTimeSizing)
-				{
-					var maxAllowed = 25;
-					double distanceToWindowBottom = zero.y - Height;
-					if (distanceToWindowBottom < 0)
-					{
-						if (Height + distanceToWindowBottom > maxAllowed)
-						{
-							MakeMenuHaveScroll(Height + distanceToWindowBottom - 5);
-						}
-						else
-						{
-							direction = Direction.Up;
-						}
-					}
-				}
-
-				switch (direction)
-				{
-					case Direction.Down:
-						this.OriginRelativeParent = zero + new Vector2(alignmentOffset, -Height) + openOffset;
-						break;
-
-					case Direction.Up:
-						this.OriginRelativeParent = zero + new Vector2(alignmentOffset, widgetRelativeTo.Height) + openOffset;
-						break;
-
-					default:
-						throw new NotImplementedException();
-				}
-			}
-
-			firstTimeSizing = false;
-		}
-
-		public override void OnMouseDown(MouseEventArgs mouseEvent)
-		{
-			scrollPositionAtMouseDown = scrollingWindow.ScrollPosition;
-			base.OnMouseDown(mouseEvent);
-		}
-
-		/// <summary>
-		/// Filter to allow click events as long as the scroll position is less than the given threshhold. Prevent click behavior on touch platfroms when drag scrolling
-		/// </summary>
-		/// <returns>A bool indicating if scroll distance is within tolerance</returns>
-		internal bool AllowClickingItems()
-		{
-			return (scrollPositionAtMouseDown - scrollPositionAtMouseUp).Length <= 5;
-		}
-
-		public override void OnMouseUp(MouseEventArgs mouseEvent)
-		{
-			bool mouseUpOnIgnoredChild = ignoredWidgets.Any(w => w.MouseCaptured || w.ChildHasMouseCaptured);
-
-			bool clickIsInsideScrollArea = (scrollingWindow?.ScrollArea?.Children?[0]?.ChildHasMouseCaptured == true);
-
-			scrollPositionAtMouseUp = scrollingWindow.ScrollPosition;
-			if (!scrollingWindow.VerticalScrollBar.ChildHasMouseCaptured
-				&& AllowClickingItems()
-				&& clickIsInsideScrollArea
-				&& !mouseUpOnIgnoredChild)
-			{
-				UiThread.RunOnIdle(CloseMenu);
-			}
-			base.OnMouseUp(mouseEvent);
-		}
-
-		public virtual void CloseMenu()
-		{
-			// Restore focus to originating widget on close
-			if (this.widgetRelativeTo != null
-				&& !widgetRelativeTo.HasBeenClosed)
-			{
-				// On menu close, select the first scrollable parent of the widgetRelativeTo
-				var scrollableParent = widgetRelativeTo.Parents<ScrollableWidget>().FirstOrDefault();
-				if (scrollableParent != null)
-				{
-					scrollableParent.Focus();
-				}
-			}
-
-			this.contentWidget?.Parent?.RemoveChild(this.contentWidget);
-			this.contentWidget.ClearRemovedFlag();
-
-			this.Parent?.RemoveChild(this);
-			this.Close();
-		}
-
-		public override void OnContainsFocusChanged(EventArgs e)
-		{
-			UiThread.RunOnIdle(() =>
-			{
-				// Fired any time focus changes. Traditionally we closed the menu if the we weren't focused. 
-				// To accommodate children (or external widgets) having focus we also query for and consider special cases
-				bool specialChildHasFocus = ignoredWidgets.Any(w => w.ContainsFocus || w.Focused)
-					|| this.ChildrenRecursive<DropDownList>().Any(w => w.IsOpen);
-
-				// If the focused changed and we've lost focus and no special cases permit, close the menu
-				if (!this.ContainsFocus
-					&& !specialChildHasFocus)
-				{
-					UiThread.RunOnIdle(CloseMenu);
-				}
-			});
-
-			base.OnContainsFocusChanged(e);
-		}
-
-		private void widgetRelativeTo_Closed(object sender, ClosedEventArgs e)
-		{
-			// If the owning widget closed, so should we
-			this.CloseMenu();
-		}
-
-		public override void OnClosed(ClosedEventArgs e)
-		{
-			// Unbind callbacks on parents for position_changed if we're closing
-			foreach (GuiWidget widget in hookedParents)
-			{
-				widget.PositionChanged -= widgetRelativeTo_PositionChanged;
-				widget.BoundsChanged -= widgetRelativeTo_PositionChanged;
-			}
-
-			// Long lived originating item must be unregistered
-			widgetRelativeTo.Closed -= widgetRelativeTo_Closed;
-
-			base.OnClosed(e);
-		}
-	}
-
-	/// <summary>
-	/// Marker interface for ignoring mouse input on popup widget children
-	/// </summary>
-	public interface IIgnoredPopupChild
-	{
 	}
 }
