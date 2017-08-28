272f9d86731a3e3f2fa71118971fa2b2399ecef3
Lars Brubaker <larsbrubaker@gmail.com>
2017-08-28 10:45:33 -0700

    New bsp and vector3 optimizations
    Fixed test

--- PolygonMesh/Processors/FaceBspTree.cs
+++ PolygonMesh/Processors/FaceBspTree.cs
@@ -1,16 +1,13 @@
 /*
 Copyright (c) 2014, Lars Brubaker
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
 1. Redistributions of source code must retain the above copyright notice, this
    list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -21,7 +18,6 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
@@ -59,7 +55,7 @@ namespace MatterHackers.PolygonMesh
 		public static void GetFacesInVisibiltyOrder(List<Face> meshFaces, BspNode node, Matrix4X4 meshToViewTransform, List<Face> faceRenderOrder)
 		{
 			// Are we in front of or behind this face
-			var faceNormalInViewSpace = Vector3.TransformNormal(meshFaces[node.Index].Normal, meshFaces[node.Index].firstFaceEdge.FirstVertex.Position, meshToViewTransform).GetNormal();
+			var faceNormalInViewSpace = Vector3.TransformNormal(meshFaces[node.Index].Normal, meshToViewTransform).GetNormal();
 			var pointOnFaceInViewSpace = Vector3.Transform(meshFaces[node.Index].firstFaceEdge.FirstVertex.Position, meshToViewTransform);
 			var infrontOfFace = Vector3.Dot(faceNormalInViewSpace, pointOnFaceInViewSpace) < 0;
 
@@ -105,46 +101,61 @@ namespace MatterHackers.PolygonMesh
 			}
 		}
 
-		private static double CalculateCrosingArrea(int faceIndex, List<Face> faces, double smallestCrossingArrea)
+		private static (double, int) CalculateCrosingArrea(int faceIndex, List<Face> faces, double smallestCrossingArrea)
 		{
 			double negativeDistance = 0;
 			double positiveDistance = 0;
+			int negativeSideCount = 0;
+			int positiveSideCount = 0;
 
 			Face checkFace = faces[faceIndex];
 			var pointOnCheckFace = faces[faceIndex].Vertices().FirstOrDefault().Position;
 
-			int step = 1; // Math.Max(1, faces.Count / 100);
-			for (int i = 0; i < faces.Count; i += step)
+			int cuts = 100;
+			int step = Math.Max(1, faces.Count / cuts);
+			for (int j = 0; j < cuts; j++)
 			{
-				if (i != faceIndex)
+				for (int i = j; i < faces.Count; i += step)
 				{
-					foreach (var vertex in faces[i].Vertices())
+					if (i < faces.Count && i != faceIndex)
 					{
-						double distanceToPlan = Vector3.Dot(checkFace.Normal, vertex.Position - pointOnCheckFace);
-						if (Math.Abs(distanceToPlan) > considerCoplaner)
+						foreach (var vertex in faces[i].Vertices())
 						{
-							if (distanceToPlan < 0)
+							double distanceToPlan = Vector3.Dot(checkFace.Normal, vertex.Position - pointOnCheckFace);
+							if (Math.Abs(distanceToPlan) > considerCoplaner)
 							{
-								// Take the square of thi distance to penalize far away points
-								negativeDistance += (distanceToPlan * distanceToPlan);
-							}
-							else
-							{
-								positiveDistance += (distanceToPlan * distanceToPlan);
-							}
+								if (distanceToPlan < 0)
+								{
+									// Take the square of thi distance to penalize far away points
+									negativeDistance += (distanceToPlan * distanceToPlan);
+								}
+								else
+								{
+									positiveDistance += (distanceToPlan * distanceToPlan);
+								}
 
-							if (negativeDistance > smallestCrossingArrea
-								&& positiveDistance > smallestCrossingArrea)
-							{
-								return negativeDistance;
+								if (negativeDistance > smallestCrossingArrea
+									&& positiveDistance > smallestCrossingArrea)
+								{
+									return (double.MaxValue, int.MaxValue);
+								}
 							}
+						}
+
+						if (negativeDistance > positiveDistance)
+						{
+							negativeSideCount++;
+						}
+						else
+						{
+							positiveSideCount++;
 						}
 					}
 				}
 			}
 
 			// return whatever side is small as our rating of badness (0 being good)
-			return Math.Min(negativeDistance, positiveDistance);
+			return (Math.Min(negativeDistance, positiveDistance), Math.Abs(negativeSideCount - positiveSideCount));
 		}
 
 		private static void CreateBackAndFrontFaceLists(int faceIndex, List<Face> faces, List<Face> backFaces, List<Face> frontFaces)
@@ -192,23 +203,28 @@ namespace MatterHackers.PolygonMesh
 
 			int bestFaceIndex = -1;
 			double smallestCrossingArrea = double.MaxValue;
+			int bestBalance = int.MaxValue;
 
 			// find the first face that does not split anything
-			int step = Math.Max(1, faces.Count / 10);
+			int step = Math.Max(1, faces.Count / 100);
 			for (int i = 0; i < faces.Count; i += step)
 			{
 				// calculate how much of polygons cross this face
-				double crossingArrea = CalculateCrosingArrea(i, faces, smallestCrossingArrea);
+				(double crossingArrea, int balance) = CalculateCrosingArrea(i, faces, smallestCrossingArrea);
 				// keep track of the best face so far
 				if (crossingArrea < smallestCrossingArrea)
 				{
 					smallestCrossingArrea = crossingArrea;
+					bestBalance = balance;
 					bestFaceIndex = i;
-					if (crossingArrea == 0)
-					{
-						// this face is good enough (not balanced but not crossing anything)
-						break;
-					}
+					break;
+				}
+				else if (crossingArrea == smallestCrossingArrea
+					&& balance < bestBalance)
+				{
+					// the crossing area is the same but the tree balance is better
+					bestBalance = balance;
+					bestFaceIndex = i;
 				}
 			}
 
