e3904219692faa02136ec4fd1ce32ee04c045ded
John Lewin <lewin76@gmail.com>
2017-10-15 16:05:09 -0700

    Increase toggle switch thumb width

--- Gui/ToggleSwitch/ToggleSwitch.cs
+++ Gui/ToggleSwitch/ToggleSwitch.cs
@@ -89,7 +89,7 @@ namespace MatterHackers.Agg.UI
 				this.Checked = startValue;
 
 				var thumbHeight = height;
-				var thumbWidth = 12;
+				var thumbWidth = 14;
 
 				InteriorColor = interiorColor;
 				ExteriorColor = exteriorColor;
