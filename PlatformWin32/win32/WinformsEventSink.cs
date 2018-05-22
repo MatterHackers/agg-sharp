e89443cc472308fcbf407581d30f358c557a7394
John Lewin <lewin76@gmail.com>
2018-05-22 06:19:08 -0700

    Use F12 to inspect

--- PlatformWin32/win32/WinformsEventSink.cs
+++ PlatformWin32/win32/WinformsEventSink.cs
@@ -58,7 +58,7 @@ namespace MatterHackers.Agg.UI
 			{
 				switch (e.KeyCode)
 				{
-					case System.Windows.Forms.Keys.F1:
+					case System.Windows.Forms.Keys.F12:
 						if (inspectForm != null)
 						{
 							// Toggle mode if window is open
