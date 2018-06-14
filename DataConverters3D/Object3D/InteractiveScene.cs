7bac099a69d290702057cbc25fb021c2c6e48dff
John Lewin <lewin76@gmail.com>
2018-06-14 15:10:35 -0700

    RebuildSuspended should not serialize to json
    - Issue MatterHackers/MCCentral#3632

--- DataConverters3D/Object3D/InteractiveScene.cs
+++ DataConverters3D/Object3D/InteractiveScene.cs
@@ -307,6 +307,7 @@ namespace MatterHackers.DataConverters3D
 
 		public bool DrawSelection { get; set; } = true;
 
+		[JsonIgnore]
 		public bool RebuildSuspended => false;
 
 		public IObject3D Clone() => sourceItem.Clone();
