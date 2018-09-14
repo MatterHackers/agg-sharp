3dfdf792b388971298bbeb1f4cef338fac5d412d
Lars Brubaker <larsbrubaker@gmail.com>
2018-09-14 09:32:27 -0700

    getting the edge renderer working again

--- RenderOpenGl/GLMeshWirePlugin.cs
+++ RenderOpenGl/GLMeshWirePlugin.cs
@@ -31,8 +31,10 @@ using MatterHackers.Agg;
 using MatterHackers.PolygonMesh;
 using MatterHackers.VectorMath;
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace MatterHackers.RenderOpenGl
 {
@@ -51,7 +53,7 @@ namespace MatterHackers.RenderOpenGl
 
 		public static string GLMeshWirePluginName => nameof(GLMeshWirePluginName);
 
-		public VectorPOD<WireVertexData> edgeLinesData = new VectorPOD<WireVertexData>();
+		public VectorPOD<WireVertexData> EdgeLines = new VectorPOD<WireVertexData>();
 
 		private int meshUpdateCount;
 		private double nonPlanarAngleRequired;
@@ -89,12 +91,62 @@ namespace MatterHackers.RenderOpenGl
 		private void CreateRenderData(Mesh meshToBuildListFor, double nonPlanarAngleRequired = 0)
 		{
 			this.nonPlanarAngleRequired = nonPlanarAngleRequired;
-			edgeLinesData = new VectorPOD<WireVertexData>();
-			// first make sure all the textures are created
+			var edgeLines = new VectorPOD<WireVertexData>();
+
+			// create a quick edge list of all the polygon edges
 			foreach (MeshEdge meshEdge in meshToBuildListFor.MeshEdges)
 			{
-				if (nonPlanarAngleRequired > 0)
+				AddVertex(edgeLines, meshEdge.VertexOnEnd[0].Position, meshEdge.VertexOnEnd[1].Position);
+			}
+			EdgeLines = edgeLines;
+
+			// if we are trying to have a filtered list do this in a backgroud thread and wait for the results
+			if (nonPlanarAngleRequired > 0)
+			{
+				Task.Run(() =>
 				{
+					var filteredEdgeLines = new VectorPOD<WireVertexData>();
+
+					List<(Vector3 start, Vector3 end, Vector3 normal, double length)> faceEdges = new List<(Vector3 start, Vector3 end, Vector3 normal, double length)>();
+					foreach (var face in meshToBuildListFor.Faces)
+					{
+						foreach (var faceEdge in face.FaceEdges())
+						{
+							var meshEdge = faceEdge.MeshEdge;
+							Vector3 start = meshEdge.VertexOnEnd[0].Position;
+							Vector3 end = meshEdge.VertexOnEnd[1].Position;
+							if (start.X > end.X || start.Y > end.Y || start.Z > end.Z)
+							{
+								var temp = start;
+								start = end;
+								end = temp;
+							}
+
+							// quantize the normals so we group them together
+							Vector3 normal = (end - start).GetNormal();
+							normal.X = ((int)(normal.X * 255)) / 255.0;
+							normal.Y = ((int)(normal.Y * 255)) / 255.0;
+							normal.Z = ((int)(normal.Z * 255)) / 255.0;
+							faceEdges.Add((start, end, normal, (end - start).Length));
+						}
+					}
+
+					// sort by direction
+					faceEdges.Sort((a, b) =>
+					{
+						if (a.normal.X == b.normal.X)
+						{
+							if (a.normal.Y == b.normal.Y)
+							{
+								return b.normal.Z.CompareTo(a.normal.Z);
+							}
+							return b.normal.Y.CompareTo(a.normal.Y);
+						}
+						return b.normal.X.CompareTo(a.normal.X);
+					});
+
+					// group face edges into co-linear segments
+
 					if (meshEdge.GetNumFacesSharingEdge() == 2)
 					{
 						FaceEdge firstFaceEdge = meshEdge.firstFaceEdge;
@@ -102,33 +154,27 @@ namespace MatterHackers.RenderOpenGl
 						double angle = Vector3.CalculateAngle(firstFaceEdge.ContainingFace.Normal, nextFaceEdge.ContainingFace.Normal);
 						if (angle > MathHelper.Tau * .1)
 						{
-							edgeLinesData.Add(AddVertex(meshEdge.VertexOnEnd[0].Position, meshEdge.VertexOnEnd[1].Position));
+							AddVertex(filteredEdgeLines, meshEdge.VertexOnEnd[0].Position, meshEdge.VertexOnEnd[1].Position);
 						}
 					}
-					else
-					{
-						edgeLinesData.Add(AddVertex(meshEdge.VertexOnEnd[0].Position, meshEdge.VertexOnEnd[1].Position));
-					}
-				}
-				else
-				{
-					edgeLinesData.Add(AddVertex(meshEdge.VertexOnEnd[0].Position, meshEdge.VertexOnEnd[1].Position));
-				}
+
+					EdgeLines = filteredEdgeLines;
+				});
 			}
 		}
 
-		private WireVertexData AddVertex(Vector3 vertex0, Vector3 vertex1)
+		private void AddVertex(VectorPOD<WireVertexData> edgeLines, Vector3 vertex0, Vector3 vertex1)
 		{
 			WireVertexData tempVertex;
 			tempVertex.positionsX = (float)vertex0.X;
 			tempVertex.positionsY = (float)vertex0.Y;
 			tempVertex.positionsZ = (float)vertex0.Z;
-			edgeLinesData.Add(tempVertex);
+			edgeLines.Add(tempVertex);
 
 			tempVertex.positionsX = (float)vertex1.X;
 			tempVertex.positionsY = (float)vertex1.Y;
 			tempVertex.positionsZ = (float)vertex1.Z;
-			return tempVertex;
+			edgeLines.Add(tempVertex);
 		}
 
 		public void Render()
