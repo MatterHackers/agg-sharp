a604e9983c495af110f10911ed91b89c79885547
jlewin <lewin76@gmail.com>
2019-06-27 10:42:27 -0700

    Extract embedded type to new file

--- agg/agg_basics.cs
+++ agg/agg_basics.cs
@@ -30,12 +30,6 @@ using MatterHackers.Agg.VertexSource;
 
 namespace MatterHackers.Agg
 {
-	public class ProgressStatus : EventArgs
-	{
-		public string Status { get; set; }
-		public double Progress0To1 { get; set; }
-	}
-
 	static public class agg_basics
 	{
 		//----------------------------------------------------------filling_rule_e
