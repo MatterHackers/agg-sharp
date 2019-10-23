a121b7bf71dd6d59a090b9d4a9484303d064100c
Lars Brubaker <larsbrubaker@gmail.com>
2019-10-22 17:41:03 -0700

    Making it possible to set the font on a drop list item

--- Gui/Menu/MenuItemStatesView.cs
+++ Gui/Menu/MenuItemStatesView.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿/*
+Copyright (c) 2019, Lars Brubaker, John Lewin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+*/
+
+using MatterHackers.Agg.Font;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -89,18 +119,20 @@ namespace MatterHackers.Agg.UI
 		public Color DisabledBackgroundColor { get; set; }
 
 		public Color NormalTextColor { get; set; }
+
 		public Color OverTextColor { get; set; }
+
 		public Color DisabledTextColor { get; set; }
 
 		public double PointSize { get { return textWidget.PointSize; } set { textWidget.PointSize = value; } }
 
-		public MenuItemColorStatesView(string name, Color textColor)
+		public MenuItemColorStatesView(string name, Color textColor, TypeFace typeFace = null)
 		{
 			HAnchor = HAnchor.Stretch | HAnchor.Fit;
 			VAnchor = VAnchor.Fit;
 			Selectable = false;
 
-			textWidget = new TextWidget(name, textColor: textColor)
+			textWidget = new TextWidget(name, textColor: textColor, typeFace: typeFace)
 			{
 				AutoExpandBoundsToText = true,
 				HAnchor = HAnchor.Left
