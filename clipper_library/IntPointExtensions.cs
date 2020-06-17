35a9393771b162da27a660855221d83637aab63b
Lars Brubaker <larsbrubaker@gmail.com>
2020-06-16 17:57:54 -0700

    fixing

--- clipper_library/IntPointExtensions.cs
+++ clipper_library/IntPointExtensions.cs
@@ -27,7 +27,6 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
-using ClipperLib;
 using MatterHackers.Agg;
 using System;
 using System.Collections.Generic;
