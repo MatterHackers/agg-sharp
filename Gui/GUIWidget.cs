602fcfed627d2c4457534291fa134250953a6188
Lars Brubaker <larsbrubaker@gmail.com>
2021-02-26 10:54:03 -0800

    Adding background rounding to guiwidget

--- Gui/GUIWidget.cs
+++ Gui/GUIWidget.cs
@@ -271,6 +271,16 @@ namespace MatterHackers.Agg.UI
 
 		public virtual int TabIndex { get; set; }
 
+		/// <summary>
+		/// The radius to use on the corners of the background and Background Outline (if enabled).
+		/// </summary>
+		public double BackgroundRadius { get; set; }
+
+		/// <summary>
+		/// Draw an outline around the background fill, this will use the OutlineColor and BackgroundRadius if set (in device units, scalled when rendered).
+		/// </summary>
+		public double BackgroundOutlineWidth { get; set; } = 0;
+
 		private Color _backgroundColor = default(Color);
 
 		public virtual Color BackgroundColor
@@ -1999,9 +2009,22 @@ namespace MatterHackers.Agg.UI
 		/// <param name="graphics2D">The graphics 2D this is being drawn onto.</param>
 		public virtual void OnDrawBackground(Graphics2D graphics2D)
 		{
+			var bounds = this.LocalBounds;
+			var rect = new RoundedRect(bounds.Left, bounds.Bottom, bounds.Right, bounds.Top, BackgroundRadius);
+
 			if (BackgroundColor.Alpha0To255 > 0)
 			{
-				graphics2D.FillRectangle(LocalBounds, BackgroundColor);
+				graphics2D.Render(rect, BackgroundColor);
+			}
+
+			if (BorderColor.Alpha0To255 > 0 && BackgroundOutlineWidth > 0)
+			{
+				var stroke = BackgroundOutlineWidth * GuiWidget.DeviceScale;
+				var expand = stroke / 2;
+				rect = new RoundedRect(bounds.Left + expand, bounds.Bottom + expand, bounds.Right - expand, bounds.Top - expand, BackgroundRadius);
+				var rectOutline = new Stroke(rect, stroke);
+
+				graphics2D.Render(rectOutline, BorderColor);
 			}
 		}
 
